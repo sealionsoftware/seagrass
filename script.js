973ed882d5e3c6486b2ef6e1fb705b54c322cb74
Richard Woods <richard@sealionsoftware.com>
2019-04-20 14:37:25 +1000

    Minor refactor

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-(function($) {
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function() {
+        let context = this, args = arguments;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+function adjustHeader() {
 
     let navigationFixedClass = 'pinned';
+
+    let $navMenu = $('.wide-menu');
+    let $header = $('.header');
+    let $logoBound = $('.logo-bound');
+    let $logo = $('.custom-logo');
+    let $graphic = $('.graphic');
+    let scrollPos = $(window).scrollTop();
+    let navTop = $navMenu.position().top;
+
+    // If the scroll is more than the custom header, set the fixed class.
+    if (scrollPos >= navTop) {
+        $header.addClass(navigationFixedClass);
+        $header.css({ top: -navTop });
+        let newHeight = $logoBound.height() - navTop;
+        $logo.height(newHeight);
+        $graphic.height(newHeight);
+    } else {
+        $header.removeClass(navigationFixedClass);
+        let newHeight = $logoBound.height() - scrollPos;
+        $logo.height(newHeight);
+        $graphic.height(newHeight);
+    }
+}
+
+(function($) {
 
     $( document ).ready( function() {
 
         if ($('.header').length){
 
-            function adjustHeader() {
-
-                let $navMenu = $('.wide-menu');
-                let $header = $('.header');
-                let $logoBound = $('.logo-bound');
-                let $logo = $('.custom-logo');
-                let $graphic = $('.graphic');
-                let scrollPos = $(window).scrollTop();
-                let navTop = $navMenu.position().top;
-
-                // If the scroll is more than the custom header, set the fixed class.
-                if (scrollPos >= navTop) {
-                    $header.addClass(navigationFixedClass);
-                    $header.css({ top: -navTop });
-                    let newHeight = $logoBound.height() - navTop;
-                    $logo.height(newHeight);
-                    $graphic.height(newHeight);
-                } else {
-                    $header.removeClass(navigationFixedClass);
-                    let newHeight = $logoBound.height() - scrollPos;
-                    $logo.height(newHeight);
-                    $graphic.height(newHeight);
-                }
-            }
-
             adjustHeader();
 
             $( window ).on( 'scroll', function() {
-                adjustHeader();
+                debounce(adjustHeader, 200, true)();
             });
 
             $( window ).resize( function() {
-                setTimeout( adjustHeader, 400 );
+                debounce( adjustHeader, 400 )();
             });
         }
 
