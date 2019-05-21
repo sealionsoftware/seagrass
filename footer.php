c913c2f384b32d7722d5176a4e54ea9f6c88b93d
Richard Woods <richard@sealionsoftware.com>
2019-05-21 12:25:37 +1000

    Fixes for IPhone styling (folded links menus) and stretched image content for cms images inside link cards

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <div class="footer">
-    <?php echo wp_nav_menu( array( 'theme_location' => 'footer-right-links', 'container_class' => 'footer-right-links', 'fallback_cb' => false ) ); ?>
-    <?php echo wp_nav_menu( array( 'theme_location' => 'footer-left-links', 'container_class' => 'footer-left-links', 'fallback_cb' => false  ) ); ?>
-    <?php dynamic_sidebar( 'footer' ); ?>
-    <div id="overlay" class="modal">
-        <?php dynamic_sidebar( 'overlay' ); ?>
+    <div class="footer-links">
+        <?php echo wp_nav_menu( array( 'theme_location' => 'footer-right-links', 'container_class' => 'footer-right-links', 'fallback_cb' => false ) ); ?>
+        <?php echo wp_nav_menu( array( 'theme_location' => 'footer-left-links', 'container_class' => 'footer-left-links', 'fallback_cb' => false  ) ); ?>
     </div>
+    <?php dynamic_sidebar( 'footer' ); ?>
+</div>
+<div id="overlay" class="modal">
+    <?php dynamic_sidebar( 'overlay' ); ?>
 </div>
