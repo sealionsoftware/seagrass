104daa3ab5327ed4fa912942f82ad2f4edd6cdaa
Richard Woods <richard@sealionsoftware.com>
2019-05-21 13:05:08 +1000

    Widescreen cards single file card highlight widescreen overlay squish

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="footer-links">
-        <?php echo wp_nav_menu( array( 'theme_location' => 'footer-right-links', 'container_class' => 'footer-right-links', 'fallback_cb' => false ) ); ?>
         <?php echo wp_nav_menu( array( 'theme_location' => 'footer-left-links', 'container_class' => 'footer-left-links', 'fallback_cb' => false  ) ); ?>
+        <?php echo wp_nav_menu( array( 'theme_location' => 'footer-right-links', 'container_class' => 'footer-right-links', 'fallback_cb' => false ) ); ?>
     </div>
     <?php dynamic_sidebar( 'footer' ); ?>
 </div>
