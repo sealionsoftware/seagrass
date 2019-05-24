a58ad53fc6240c5d9324634ae26398d122c403ab
Richard Woods <richard@sealionsoftware.com>
2019-05-24 14:40:12 +1000

    Restructure page to always have link-card section for child pages (remove feature-on-home query and rely on page structure instead)

--- page.php
+++ page.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <?php
     $description=get_post_custom_values('description');
-    if (is_front_page()) :
-        $featuredPages = new WP_Query(array(
-            'post_type' => 'page',
-            'meta_key' => 'feature-on-home',
-            'meta_value' => 'true'
-        ));
-    endif;
+    $childPages = new WP_Query(array(
+        'post_type' => 'page',
+        'post_parent'       => $post->ID
+    ));
 ?>
 <html lang="en">
     <?php get_header('head');?>
@@ -27,16 +24,14 @@
             <?php if (have_posts()) : while (have_posts()) : ?>
                 <?php the_post(); the_content(); ?>
             <?php endwhile; endif; ?>
-            <?php if (isset($featuredPages )) : ?>
-                <div class="link-cards">
-                    <?php while ($featuredPages->have_posts()) : $featuredPages->the_post() ?>
-                        <div>
-                            <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-                            <?php the_content('');?>
-                        </div>
-                    <?php endwhile; wp_reset_postdata();?>
-                </div>
-            <?php endif;?>
+            <div class="link-cards">
+                <?php while ($childPages->have_posts()) : $childPages->the_post() ?>
+                    <div>
+                        <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+                        <?php the_content('');?>
+                    </div>
+                <?php endwhile; wp_reset_postdata();?>
+            </div>
             <?php get_footer(); ?>
         </div>
         <?php wp_footer(); ?>
