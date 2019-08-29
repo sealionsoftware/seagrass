d0486dca8fd517175dbcfdf105e3fa34fcf3f5dc
Richard Woods <richard@sealionsoftware.com>
2019-08-29 11:50:17 +1000

    Clean up new page css

--- page.php
+++ page.php
@@ -2,11 +2,13 @@
 <html lang="en">
     <?php get_header('head');?>
     <body>
-        <?php get_header(); ?>
-        <?php if (have_posts()) : while (have_posts()) : ?>
-            <?php the_post(); the_content(); ?>
-        <?php endwhile; endif; ?>
-        <?php get_footer(); ?>
+        <div class="content">
+            <?php get_header(); ?>
+            <?php if (have_posts()) : while (have_posts()) : ?>
+                <?php the_post(); the_content(); ?>
+            <?php endwhile; endif; ?>
+            <?php get_footer(); ?>
+        </div>
         <?php wp_footer(); ?>
     </body>
 </html>
