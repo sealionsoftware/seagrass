fa1e488cbc1444cee727ad8c3f4b8afcabaf22ab
Richard Woods <richard@sealionsoftware.com>
2020-01-31 11:28:18 +1000

    Revert "Established a "copy" area in the templates where all links appear in the highlight color by default. These areas are neutral backed in Seagrass. Previously we were just targetting a links in paragraphs - but this missed links in cite, li etc while risking picking up links in non-neutral header/footer areas that use <p> for whatever reason."
    This reverts commit e7b64df5

--- page.php
+++ page.php
@@ -4,11 +4,9 @@
     <body>
         <div class="content">
             <?php get_header(); ?>
-            <div class="copy">
-                <?php if (have_posts()) : while (have_posts()) : ?>
-                    <?php the_post(); the_content(); ?>
-                <?php endwhile; endif; ?>
-            </div>
+            <?php if (have_posts()) : while (have_posts()) : ?>
+                <?php the_post(); the_content(); ?>
+            <?php endwhile; endif; ?>
             <?php get_footer(); ?>
         </div>
         <?php wp_footer(); ?>
