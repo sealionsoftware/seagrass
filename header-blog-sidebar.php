4231371051181307363aa6af8a1605ab5a864b18
Richard Woods <richard@sealionsoftware.com>
2020-02-08 18:30:18 +1000

    Better layout for feed including groups in the sidebar

--- header-blog-sidebar.php
+++ header-blog-sidebar.php
@@ -5,12 +5,14 @@ $childPages = new WP_Query(array(
 $year = null;
 ?>
 <div class="sidebar">
+    <div>
     <?php while ($childPages->have_posts()) :
         $childPages->the_post() ;
         $postYear = get_the_date('Y') ;
         if ($postYear !== $year):
             $year = $postYear;
             ?>
+    </div><div class="sidebar-group">
             <h2><?php echo $postYear?></h2>
         <?php endif; ?>
         <div class="article-link">
@@ -18,11 +20,14 @@ $year = null;
             <span class="article-date"><?php the_date('F j');?></span>
         </div>
     <?php endwhile; wp_reset_postdata();?>
+    </div>
 
     <?php
     if ( has_nav_menu( 'external-article-links' ) ): ?>
+    <div class="sidebar-group">
         <h2>Articles</h2>
-        <?php echo wp_nav_menu( array( 'theme_location' => 'external-article-links', 'container_class' => 'external-article-links', 'fallback_cb' => false ) );
-    endif;
-    ?>
+        <?php echo wp_nav_menu( array( 'theme_location' => 'external-article-links', 'container_class' => 'external-article-links', 'fallback_cb' => false ) ); ?>
+    </div>
+
+    <?php endif; ?>
 </div>
